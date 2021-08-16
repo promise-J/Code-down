4221a6cea1c226e318ff8ccb66cc0949f416bea3
Promise Johnson <chiemelapromise30@gmail.com>
2021-08-16 10:15:57 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Code-down-camp
---
+++ questions.js
@@ -0,0 +1,646 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+ 
+];
+
+
+
+
+
+//day one
+const questions1 = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is img tag used for?",
+    answer: "Image display in the web",
+    options: [
+      "Setting the web running",
+      "Image display in the web",
+      "Making the code better",
+      "Displaying tags in the web page"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What of the following is not an Element?",
+    answer: "Alt",
+    options: [
+      "P",
+      "Ul",
+      "Br",
+      "Alt"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does src represents?",
+    answer: "Source",
+    options: [
+      "Search",
+      "Server",
+      "Source",
+      "Scratch"
+    ]
+  },
+  {
+    numb: 5,
+    question: "How many heading tags do we have?",
+    answer: "6",
+    options: [
+      "4",
+      "6",
+      "7",
+      "5"
+    ]
+  },
+  {
+    numb: 6,
+    question: "A document displayed on the web is called?",
+    answer: "Web page",
+    options: [
+      "Web server",
+      "Web browser",
+      "Web display",
+      "Web page"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The current version of html is?",
+    answer: "HTML5",
+    options: [
+      "XHTML",
+      "XML",
+      "HTTP",
+      "HTML5"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Styling inside a html tag is called?",
+    answer: "Inline styling",
+    options: [
+      "In-styling",
+      "Inside styling",
+      "Internal styling",
+      "Inline styling"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The i tag stands for?",
+    answer: "Italics",
+    options: [
+      "Italics",
+      "Internal",
+      "Interval",
+      "Iteration"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The sematic order of HTML is?",
+    answer: "header body footer",
+    options: [
+      "title page body",
+      "nav body footer",
+      "page title body",
+      "header body footer"
+    ]
+  },
+
+]
+
+
+//day 2
+const questions2 = [
+  {
+    numb: 1,
+    question: "What does CSS stand for?",
+    answer: "Cascading style sheet",
+    options: [
+      "Cross selection style",
+      "Cascading style sheet",
+      "Coloring style sheet",
+      "Cascading selection sheet"
+    ]
+  },
+  {
+    numb: 2,
+    question: "The CSS property that changes text size is called?",
+    answer: "Font size",
+    options: [
+      "Text resize",
+      "Font family",
+      "Font size",
+      "Text size"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Rgb represents?",
+    answer: "Red green blue",
+    options: [
+      "Resizing general body",
+      "Red green blue",
+      "Rare green body",
+      "Red green body"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does src represents?",
+    answer: "Source",
+    options: [
+      "Search",
+      "Server",
+      "Source",
+      "Scratch"
+    ]
+  },
+  {
+    numb: 5,
+    question: "How many heading tags do we have?",
+    answer: "6",
+    options: [
+      "4",
+      "6",
+      "7",
+      "5"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Hex decimal is used in _____",
+    answer: "Color",
+    options: [
+      "Color",
+      "Numbers",
+      "Images",
+      "Alpha-numeric"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following is not a pseudo class ?",
+    answer: "Anchor",
+    options: [
+      "Hover",
+      "After",
+      "Focus",
+      "Anchor"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which is the following denotes id and class?",
+    answer: "# and .",
+    options: [
+      "# and .",
+      "# and %",
+      "* and #",
+      ". and &"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following is a universal selector?",
+    answer: "*",
+    options: [
+      "#-",
+      "*",
+      "#",
+      "all::"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Margin in CSS is used to create",
+    answer: "Space around a box",
+    options: [
+      "Space inside a box",
+      "Space in a box",
+      "Space around a box",
+      "Space away from a box"
+    ]
+  },
+
+]
+
+
+
+//day 3
+const questions3 = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is img tag used for?",
+    answer: "Image display in the web",
+    options: [
+      "Setting the web running",
+      "Image display in the web",
+      "Making the code better",
+      "Displaying tags in the web page"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What of the following is not an Element?",
+    answer: "Alt",
+    options: [
+      "P",
+      "Ul",
+      "Br",
+      "Alt"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does src represents?",
+    answer: "Source",
+    options: [
+      "Search",
+      "Server",
+      "Source",
+      "Scratch"
+    ]
+  },
+  {
+    numb: 5,
+    question: "How many heading tags do we have?",
+    answer: "6",
+    options: [
+      "4",
+      "6",
+      "7",
+      "5"
+    ]
+  },
+  {
+    numb: 6,
+    question: "A document displayed on the web is called?",
+    answer: "Web page",
+    options: [
+      "Web server",
+      "Web browser",
+      "Web display",
+      "Web page"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The current version of html is?",
+    answer: "HTML5",
+    options: [
+      "XHTML",
+      "XML",
+      "HTTP",
+      "HTML5"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Styling inside a html tag is called?",
+    answer: "Inline styling",
+    options: [
+      "In-styling",
+      "Inside styling",
+      "Internal styling",
+      "Inline styling"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The i tag stands for?",
+    answer: "Italics",
+    options: [
+      "Italics",
+      "Internal",
+      "Interval",
+      "Iteration"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The sematic order of HTML is?",
+    answer: "header body footer",
+    options: [
+      "title page body",
+      "nav body footer",
+      "page title body",
+      "header body footer"
+    ]
+  },
+
+]
+
+
+
+//day4
+
+const questions4 = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is img tag used for?",
+    answer: "Image display in the web",
+    options: [
+      "Setting the web running",
+      "Image display in the web",
+      "Making the code better",
+      "Displaying tags in the web page"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What of the following is not an Element?",
+    answer: "Alt",
+    options: [
+      "P",
+      "Ul",
+      "Br",
+      "Alt"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does src represents?",
+    answer: "Source",
+    options: [
+      "Search",
+      "Server",
+      "Source",
+      "Scratch"
+    ]
+  },
+  {
+    numb: 5,
+    question: "How many heading tags do we have?",
+    answer: "6",
+    options: [
+      "4",
+      "6",
+      "7",
+      "5"
+    ]
+  },
+  {
+    numb: 6,
+    question: "A document displayed on the web is called?",
+    answer: "Web page",
+    options: [
+      "Web server",
+      "Web browser",
+      "Web display",
+      "Web page"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The current version of html is?",
+    answer: "HTML5",
+    options: [
+      "XHTML",
+      "XML",
+      "HTTP",
+      "HTML5"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Styling inside a html tag is called?",
+    answer: "Inline styling",
+    options: [
+      "In-styling",
+      "Inside styling",
+      "Internal styling",
+      "Inline styling"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The i tag stands for?",
+    answer: "Italics",
+    options: [
+      "Italics",
+      "Internal",
+      "Interval",
+      "Iteration"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The sematic order of HTML is?",
+    answer: "header body footer",
+    options: [
+      "title page body",
+      "nav body footer",
+      "page title body",
+      "header body footer"
+    ]
+  },
+
+]
+
+
+//day 5
+const questions5 = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is img tag used for?",
+    answer: "Image display in the web",
+    options: [
+      "Setting the web running",
+      "Image display in the web",
+      "Making the code better",
+      "Displaying tags in the web page"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What of the following is not an Element?",
+    answer: "Alt",
+    options: [
+      "P",
+      "Ul",
+      "Br",
+      "Alt"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does src represents?",
+    answer: "Source",
+    options: [
+      "Search",
+      "Server",
+      "Source",
+      "Scratch"
+    ]
+  },
+  {
+    numb: 5,
+    question: "How many heading tags do we have?",
+    answer: "6",
+    options: [
+      "4",
+      "6",
+      "7",
+      "5"
+    ]
+  },
+  {
+    numb: 6,
+    question: "A document displayed on the web is called?",
+    answer: "Web page",
+    options: [
+      "Web server",
+      "Web browser",
+      "Web display",
+      "Web page"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The current version of html is?",
+    answer: "HTML5",
+    options: [
+      "XHTML",
+      "XML",
+      "HTTP",
+      "HTML5"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Styling inside a html tag is called?",
+    answer: "Inline styling",
+    options: [
+      "In-styling",
+      "Inside styling",
+      "Internal styling",
+      "Inline styling"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The i tag stands for?",
+    answer: "Italics",
+    options: [
+      "Italics",
+      "Internal",
+      "Interval",
+      "Iteration"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The sematic order of HTML is?",
+    answer: "header body footer",
+    options: [
+      "title page body",
+      "nav body footer",
+      "page title body",
+      "header body footer"
+    ]
+  },
+
+]
+
+
